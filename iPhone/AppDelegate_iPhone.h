d73269a834b2e0c50a9057718992e40179053d97
tfausak <tfausak@famigogames.com>
2010-10-25 14:06:15 -0500

    It doesn't do anyting, but it compiles and runs!

---
+++ iPhone/AppDelegate_iPhone.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPhone.h
+//  Aardvark
+//
+//  Created by Taylor Fausak on 10/25/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPhone : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
