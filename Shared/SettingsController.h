f7aac53539bf7cca54017ae5fa70adb9e3976d89
tfausak <tfausak@famigogames.com>
2010-11-12 15:50:28 -0600

    Removed Facebook share stuff and added the beginnings of Famigo share stuff.

--- Shared/SettingsController.h
+++ Shared/SettingsController.h
@@ -8,37 +8,27 @@
 
 #import <UIKit/UIKit.h>
 #import "FamigoHeaders.h"
-#import "FBConnect.h"
 
-@interface SettingsController : UIViewController <FBRequestDelegate, FBDialogDelegate, FBSessionDelegate> {
+@interface SettingsController : UIViewController {
     UIViewController *parentViewController;
-    Facebook *facebook;
-    BOOL loggedIntoFacebook;
     
     // UI elements
     UINavigationBar *navigationBar;
     UISlider *difficultySlider;
     UISwitch *allowNegativeNumbersSwitch;
-    UIButton *facebookButton;
     UIButton *shareButton;
-    UIButton *linkButton;
 }
 
 @property (nonatomic, assign) UIViewController *parentViewController;
-@property (nonatomic, retain) Facebook *facebook;
 @property (nonatomic, assign) BOOL loggedIntoFacebook;
 @property (nonatomic, retain) UINavigationBar *navigationBar;
 @property (nonatomic, retain) UISlider *difficultySlider;
 @property (nonatomic, retain) UISwitch *allowNegativeNumbersSwitch;
-@property (nonatomic, retain) UIButton *facebookButton;
 @property (nonatomic, retain) UIButton *shareButton;
-@property (nonatomic, retain) UIButton *linkButton;
 
 - (void)movedSlider:(id)sender;
 - (void)toggledSwitch:(id)sender;
-- (void)pressedFacebookButton:(id)sender;
 - (void)pressedShareButton:(id)sender;
-- (void)pressedLinkButton:(id)sender;
 - (void)drawUI;
 - (void)updateUI;
 
--- Shared/SettingsController.m
+++ Shared/SettingsController.m
@@ -11,24 +11,18 @@
 @implementation SettingsController
 
 @synthesize parentViewController;
-@synthesize facebook;
 @synthesize loggedIntoFacebook;
 @synthesize navigationBar;
 @synthesize difficultySlider;
 @synthesize allowNegativeNumbersSwitch;
-@synthesize facebookButton;
 @synthesize shareButton;
-@synthesize linkButton;
 
 - (void)dealloc {
     [parentViewController release];
-    [facebook release];
     [navigationBar release];
     [difficultySlider release];
     [allowNegativeNumbersSwitch release];
-    [facebookButton release];
     [shareButton release];
-    [linkButton release];
     
     [super dealloc];
 }
@@ -44,7 +38,6 @@
 
 - (void)viewDidLoad {
     [[self view] setBackgroundColor:[UIColor lightGrayColor]];
-    facebook = [[Facebook alloc] init];
     
     navigationBar = [[UINavigationBar alloc] init]; {
         UINavigationItem *navigationItem = [[UINavigationItem alloc] init];
@@ -82,17 +75,10 @@
         [allowNegativeNumbersSwitch setOn:allowNegativeNumbers];
     } [[self view] addSubview:allowNegativeNumbersSwitch];
     
-    facebookButton = [UIButton buttonWithType:UIButtonTypeRoundedRect]; {
-        [facebookButton addTarget:self action:@selector(pressedFacebookButton:) forControlEvents:UIControlEventTouchUpInside];
-    } [[self view] addSubview:facebookButton];
-    
     shareButton = [UIButton buttonWithType:UIButtonTypeRoundedRect]; {
         [shareButton addTarget:self action:@selector(pressedShareButton:) forControlEvents:UIControlEventTouchUpInside];
+        [shareButton setTitle:@"Share" forState:UIControlStateNormal];
     } [[self view] addSubview:shareButton];
-    
-    linkButton = [UIButton buttonWithType:UIButtonTypeRoundedRect]; {
-        [linkButton addTarget:self action:@selector(pressedLinkButton:) forControlEvents:UIControlEventTouchUpInside];
-    } [[self view] addSubview:linkButton];
     
     [self drawUI];
 }
@@ -111,52 +97,7 @@
     [defaults synchronize];    
 }
 
-- (void)pressedFacebookButton:(id)sender {
-    if (loggedIntoFacebook) {
-        [facebook logout:self];
-    }
-    else {
-        NSArray *permissions = [[NSArray arrayWithObjects:@"publish_stream", nil] retain];
-        [facebook authorize:FamigoFacebookApplicationID permissions:permissions delegate:self];
-    }
-}
-
 - (void)pressedShareButton:(id)sender {
-    NSDictionary *actionLinks = [NSArray arrayWithObjects:[NSDictionary dictionaryWithObjectsAndKeys:
-                                                           @"Famigo", @"text",
-                                                           @"http://www.famigogames.com/", @"href",
-                                                           nil], nil];
-    NSDictionary *media = [NSArray arrayWithObjects:[NSDictionary dictionaryWithObjectsAndKeys:
-                                                     @"image", @"type",
-                                                     @"http://famigogames.com/imgs/violet_bg.png", @"src",
-                                                     @"http://www.famigogames.com/#image", @"href",
-                                                     nil], nil];
-    NSDictionary *attachment = [NSDictionary dictionaryWithObjectsAndKeys:
-                                @"Rad Ribs", @"name",
-                                @"I finished a Rab Rib called 'The Puppy Park'!", @"caption",
-                                @"One summer, my gramps and I took a frumpy trip to couch. It took us seventeen hours to get there! Along the way, we stopped to see the freezing gazelle. When I tried to take a picture of it, it started to wince and think. After that, we stopped in bedroom. That place is really sexy - I think I want to move there! When we finally got to our destination, I just couldn't help myself - I ran out of the car and snogged. It was so tiny, and at the gift shop, I bought myself two bra. What a great trip!", @"description",
-                                @"http://www.famigogames.com/#attachment", @"href",
-                                media, @"media",
-                                nil];
-    
-    SBJSON *jsonWriter = [[SBJSON new] autorelease];
-    NSMutableDictionary* params = [NSMutableDictionary dictionaryWithObjectsAndKeys:
-                                   FamigoFacebookApplicationID, @"api_key",
-                                   @"Share on Facebook",  @"user_message_prompt",
-                                   [jsonWriter stringWithObject:actionLinks], @"action_links",
-                                   [jsonWriter stringWithObject:attachment], @"attachment",
-                                   nil];
-
-    [facebook dialog:@"stream.publish" andParams:params andDelegate:self];
-}
-
-- (void)pressedLinkButton:(id)sender {
-    NSString *path = @"http://www.famigogames.com/main/viewfinishedgame?family_id=family_ee9084c1c2a7e50f81456d04d8ce2f9a4b61ebf3&game_id=game_97f0d3b95d3e0b96ad98f36e59db5530a7f61e32";
-    
-    NSMutableDictionary *params = [NSMutableDictionary dictionaryWithObjectsAndKeys:
-                                   path, @"url",
-                                   nil];
-    [facebook requestWithMethodName:@"links.post" andParams:params andHttpMethod:@"POST" andDelegate:self];
 }
 
 #pragma mark -
@@ -189,54 +130,12 @@
     
     [allowNegativeNumbersSwitch setFrame:CGRectMake(([[self view] frame].size.width / 2) - 47, 106, 94, 27)];
     
-    [facebookButton setFrame:CGRectMake(([[self view] frame].size.width / 2) - 36, 153, 72, 37)];
-    
-    [shareButton setFrame:CGRectMake(([[self view] frame].size.width / 2) - 36, 210, 72, 37)];
-    
-    [linkButton setFrame:CGRectMake(([[self view] frame].size.width / 2) - 36, 267, 72, 37)];
+    [shareButton setFrame:CGRectMake(([[self view] frame].size.width / 2) - 36, 153, 72, 37)];
     
     [self updateUI];
 }
 
 - (void)updateUI {
-    if (loggedIntoFacebook) {
-        [facebookButton setTitle:@"logout" forState:UIControlStateNormal];
-        [shareButton setTitle:@"share" forState:UIControlStateNormal];
-        [linkButton setTitle:@"link" forState:UIControlStateNormal];
-    }
-    else {
-        [facebookButton setTitle:@"login" forState:UIControlStateNormal];
-        [shareButton setTitle:@"-" forState:UIControlStateNormal];
-        [linkButton setTitle:@"-" forState:UIControlStateNormal];
-    }
-}
-
-#pragma mark -
-#pragma mark Facebook
-
-- (void)request:(FBRequest *)request didReceiveResponse:(NSURLResponse *)response {
-}
-
-- (void)request:(FBRequest *)request didLoad:(id)result {
-}
-
-- (void)request:(FBRequest *)request didFailWithError:(NSError *)error {
-}
-
-- (void)dialogDidComplete:(FBDialog *)dialog {
-}
-
-- (void)fbDidLogin {
-    loggedIntoFacebook = YES;
-    [self updateUI];
-}
-
-- (void)fbDidLogout {
-    loggedIntoFacebook = NO;
-    [self updateUI];
-}
-
-- (void)fbDidNotLogin:(BOOL)cancelled {
 }
 
 @end
