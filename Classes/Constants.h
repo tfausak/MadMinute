c857f232b6de0b1f6ca2b09ae193a1a00b5cbc27
tfausak <tfausak@famigogames.com>
2011-01-11 10:47:10 -0600

    Fixed the API key.

--- Classes/Constants.h
+++ Classes/Constants.h
@@ -6,7 +6,7 @@
 //  Copyright 2010 __MyCompanyName__. All rights reserved.
 //
 
-#define kAPIKey @"0aa3a9f0acf8308151a50b8dd5d12e06a323ab12"
+#define kAPIKey @"88bc1871153b792e827d116603f0400db906236c"
 #define kFamigoIPAddress @"174.143.213.31"
 #define kGameName @"Mad Minute"
 #define kGameInstructions @"\nIt's like math, but faster!\n\n"
